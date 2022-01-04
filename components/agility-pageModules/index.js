1a7b5eff2ea145498fe848e14cb1536c0df92611
codershubham350 <shubhamsaxena350@gmail.com>
2022-01-04 16:48:11 +0530

    Changes in Homepage

--- components/agility-pageModules/index.js
+++ components/agility-pageModules/index.js
@@ -4,9 +4,9 @@ import PostsListing from "./PostsListing";
 import PostDetails from "./PostDetails";
 import Heading from "./Heading";
 import Footer from "./Footer/Footer";
-import FooterContent1 from "./Footer/FooterContent1";
-import FooterContent2 from "./Footer/FooterContent2";
-import FooterContent3 from "./Footer/FooterContent3";
+// import FooterContent1 from "./Footer/FooterContent1";
+// import FooterContent2 from "./Footer/FooterContent2";
+// import FooterContent3 from "./Footer/FooterContent3";
 // import CampusImageSlider from "./Image-Slider/CampusImageSlider";
 import Header from "../Header/Header";
 import ImageSlider from "./Image-Slider/ImageSlider";
@@ -20,9 +20,9 @@ const allModules = [
   { name: "Footer", module: Footer },
   { name: "Header", module: Header },
   // { name: "CampusImageSlider", module: CampusImageSlider },
-  { name: "FooterContent1", module: FooterContent1 },
-  { name: "FooterContent2", module: FooterContent2 },
-  { name: "FooterContent3", module: FooterContent3 },
+  // { name: "FooterContent1", module: FooterContent1 },
+  // { name: "FooterContent2", module: FooterContent2 },
+  // { name: "FooterContent3", module: FooterContent3 },
   { name: "ImageSlider", module: ImageSlider },
   { name: "Heading", module: Heading },
   { name: "FeaturedPost", module: FeaturedPost },
